c4dc2c1e6a8ce044ebe4a5ae9a37166bf29ce5a7
Valerio <canivalerio93@gmail.com>
2022-09-23 14:50:08 +0200

    ora ha senso

--- index.php
+++ index.php
@@ -7,34 +7,41 @@ class Movie {
     public $review;
     public $movie_age;
 
-
     public function __construct($Title, $Director, $Release_Year, $Review){
-        echo    "<ul>
-                    <li> Titolo: 
-                        $Title 
-                    </li>  
-                    <li> Diretto da 
-                        $Director 
-                    </li>
-                    <li> Uscito nelle sale nel 
-                        $Release_Year 
-                    </li>
-                    <li> Con un voto complessivo di 
-                        $Review 
-                    </li>   
-                </ul>";
+        $this->title = $Title;
+        $this->director = $Director;
+        $this->release_year = $Release_Year;
+        $this->review = $Review;
     } 
 
-    public function getMovieAge($release_year){
-        $movie_age = 2022 - $release_year;
-        echo "<h2>This Movie is $movie_age years old!!</h2>";
+    public function visualizeMovie(){
+        echo    "<ul>
+        <li> Titolo: 
+            $this->title 
+        </li>  
+        <li> Diretto da 
+            $this->director 
+        </li>
+        <li> Uscito nelle sale nel 
+            $this->release_year 
+        </li>
+        <li> Con un voto complessivo di 
+            $this->review 
+        </li>   
+    </ul>";
+    }
 
+    public function getMovieAge(){
+        $movie_age = 2022 - $this->release_year;
+        echo "<h2>This Movie is $movie_age years old!!</h2>";
     }
 }
 
 
 $Avatar = new Movie("Avatar", "James Cameron", 2009, 7.5);
-$Avatar->getMovieAge(2009);
+$Avatar->visualizeMovie(); 
+$Avatar->getMovieAge();
 
 $NoCountryForOldMan = new Movie("NoCountryForOldMan", "Joel Coen", 2007, 7.9);
-$NoCountryForOldMan->getMovieAge(2007);
+$NoCountryForOldMan->visualizeMovie(); 
+$NoCountryForOldMan->getMovieAge();
